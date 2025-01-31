2a189494be9fdb8388755244c7c67f330a1f44e1
Mayank Goyal <45072941+4gmayank@users.noreply.github.com>
2025-01-31 08:37:39 +0400

    Update media_gal.dart

--- lib/media_gal.dart
+++ lib/media_gal.dart
@@ -5,4 +5,41 @@ class MediaGal {
   Future<String?> getPlatformVersion() {
     return MediaGalPlatform.instance.getPlatformVersion();
   }
+  static const platform = MethodChannel('com.example.media'); // Channel name should match with native code
+
+  static Future<void> showToast() async {
+    try {
+
+      await platform.invokeMethod('showToast', {'message': 'Brother, Click Attachment send Media!'});
+    } on PlatformException catch (e) {
+      print("Failed to show toast: ${e.message}");
+    }
+  }
+
+    static Future<List<Object?>> loadMediaList() async {
+    try {
+
+      var files = await platform.invokeMethod('gallery', {'message': 'Brother, Click Attachment send Media!'});
+      print(files.toString());
+      print(files.length);
+      return files;
+    } on PlatformException catch (e) {
+      print("Failed to show toast: ${e.message}");
+    }
+    return [];
+  }
+
+    Future<void> _loadAssets() async {
+    // Request permissions for photos
+    await Permission.photos.request();
+    List<Object?> path1 = await Config.loadMediaList();
+
+    for (var test in path1) {
+      if(test is String?){
+        if(test != null && test.isNotEmpty){
+          this.path.add(test+test);
+        }
+      }
+    }
+    }
 }
